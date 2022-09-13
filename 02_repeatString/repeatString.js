d8526b9febc0c558591197281a9ba558d5a6b29a
unknown <bengera89@gmail.com>
2022-09-13 15:06:06 +0700

    repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = '';
+    if (num < 0) {
+        newString = 'ERROR';
+        
+    }
+   
+        while (num > 0) {
+            newString += string;
+            num--;
+        }
+        
+        
+        return newString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
