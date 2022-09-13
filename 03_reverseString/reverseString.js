617b6907acbaf927275a13cea209979b37985de3
unknown <bengera89@gmail.com>
2022-09-13 17:19:48 +0700

    reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("") 
+      splitString.reverse();
+      let joinString = splitString.join();
+      let result = joinString.replace(/,/g,'');
+      return result;
 };
 
 // Do not edit below this line
