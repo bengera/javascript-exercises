6fee07f4bf30a8150bd4bc818a30db698a37ecbf
unknown <bengera89@gmail.com>
2022-09-15 16:36:46 +0700

    removeArrayFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (array, ...args) { //args becomes an array
+    args.forEach((arg) => { // for each array value
+       const index = array.indexOf(arg); // find index of value given
+       if (index > -1) {
+          array.splice(index, 1) // at the position of argument, remove 1 
+       }
+       
+    })
+    return array;
+    
+ };
 
 // Do not edit below this line
 module.exports = removeFromArray;
