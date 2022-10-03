15709c9a8dc21f38ba274a6351462f496c7a7a65
Ben <bengeraghty.bg@gmail.com>
2022-10-03 14:26:34 +0700

    tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,27 @@
-const ftoc = function() {
+const ftoc = function (temp) { // farenheit to celcius
+  let divisionFartoCel = 5 / 9;
+  temp = (temp - 32) * divisionFartoCel;
+  if (temp % 1 != 0) {
+    let rounded = temp.toFixed(1);
+    return parseFloat(rounded);
+  } else {
+    return temp;
+  }
+
+
 
 };
 
-const ctof = function() {
-
+const ctof = function (temp) { // celcius to farenheit
+  let divisionCeltoFar = 9 / 5;
+  temp = temp * divisionCeltoFar + 32;
+  if (temp % 1 != 0) {
+    let rounded = temp.toFixed(1);
+    return parseFloat(rounded);
+  } else {
+    return temp;
+  }
 };
-
 // Do not edit below this line
 module.exports = {
   ftoc,
