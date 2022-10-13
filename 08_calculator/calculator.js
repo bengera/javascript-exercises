622517eaa788c9cc938a18c5d80a989decddb5d9
Ben <bengeraghty.bg@gmail.com>
2022-10-13 11:02:14 +0700

    palindromes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(n1,n2,...n) {
+  return parseInt(n1 + n2 + n);
 };
 
-const subtract = function() {
-	
+const subtract = function(n1,n2,...n) {
+return n1 - n2 - n;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+let sum = 0;
+for (let i = 0; i < arr.length; i++){
+  sum += arr[i];
+  
+}
+return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+let sum = 1; 
+for (let i = 0; i < arr.length; i++){
+  sum *= arr[i];
+  
+}
+return sum;
 };
 
-const power = function() {
-	
+const power = function(n1,n2,...n) {
+return Math.pow(n1,n2,n);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+let result = 1;
+if (n === 0 || n ===1){
+  return 1;
+} else {
+  return n * factorial(n-1);
+}
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
-};
+    const regex = (/[^a-z0-9_]/g);
+    const lowRegStr = str.toLowerCase().replace(regex, '');
+    const reverseStr = lowRegStr.split('').reverse().join('');
+  
+    return reverseStr === lowRegStr;
+  
+  
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
